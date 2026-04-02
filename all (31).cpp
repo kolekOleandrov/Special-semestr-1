4ab8e78339e6255a88b10d85ab91c25a2b7aa532
kolekOleandrov <governmentnana@gmail.com>
2026-04-03 02:19:05 +0300

    Add files via upload

---
+++ all (31).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Введите двухзначное число: ";
+    cin >> n;
+    
+    int d1 = n / 10;           // первая цифра
+    int d2 = n % 10;           // вторая цифра
+    int s = d1 + d2;           // сумма цифр
+    
+    if (s % 3 == 0) {
+        cout << "Сумма цифр кратна 3" << endl;
+    } else {
+        cout << "Сумма цифр не кратна 3" << endl;
+    }
+    
+    return 0;
+}
